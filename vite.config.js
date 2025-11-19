de95320bc5b8ecb31323f41521c47b383234e2b5
이희원 <richready2011@gmail.com>
2025-11-19 09:24:10 +0900

    env: vite 포트변경

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     host: '0.0.0.0',
-    port: 5173,
+    port: 5000,
     hmr: {
-      host: '5173.devtool.codeserver.diffthink.kr',
+      host: '5000.devtool.codeserver.diffthink.kr',
       protocol: 'ws'
     }
   },
